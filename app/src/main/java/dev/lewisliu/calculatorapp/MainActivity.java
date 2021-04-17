fee5d62925ddefa2a2bb14a61a911c1af3329cf0
Lewis Liu <lewisliuusa@outlook.com>
2021-04-17 13:56:53 -0700

    started working on cal logics

--- app/src/main/java/dev/lewisliu/calculatorapp/MainActivity.java
+++ app/src/main/java/dev/lewisliu/calculatorapp/MainActivity.java
@@ -3,8 +3,13 @@ package dev.lewisliu.calculatorapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText result;
+    private EditText newNum;
+    private TextView displayOperation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
