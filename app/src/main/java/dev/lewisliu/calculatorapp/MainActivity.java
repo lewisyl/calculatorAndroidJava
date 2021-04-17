eae8a3904632226135ce111272125d9241304482
Lewis Liu <lewisliuusa@outlook.com>
2021-04-17 14:12:42 -0700

    added buttons

--- app/src/main/java/dev/lewisliu/calculatorapp/MainActivity.java
+++ app/src/main/java/dev/lewisliu/calculatorapp/MainActivity.java
@@ -3,6 +3,7 @@ package dev.lewisliu.calculatorapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -10,10 +11,36 @@ public class MainActivity extends AppCompatActivity {
     private EditText result;
     private EditText newNum;
     private TextView displayOperation;
+    // Variables to hold the operands and type of calulations
+    private Double operand1 = null;
+    private Double operand2 = null;
+    private String pendingOperation = "=";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        result = (EditText) findViewById(R.id.result);
+        newNum = (EditText) findViewById(R.id.newNumber);
+        displayOperation = (TextView) findViewById(R.id.operation);
+
+        Button button0 = (Button) findViewById(R.id.button0);
+        Button button1 = (Button) findViewById(R.id.button1);
+        Button button2 = (Button) findViewById(R.id.button2);
+        Button button3 = (Button) findViewById(R.id.button3);
+        Button button4 = (Button) findViewById(R.id.button4);
+        Button button5 = (Button) findViewById(R.id.button5);
+        Button button6 = (Button) findViewById(R.id.button6);
+        Button button7 = (Button) findViewById(R.id.button7);
+        Button button8 = (Button) findViewById(R.id.button8);
+        Button button9 = (Button) findViewById(R.id.button9);
+        Button buttonDot = (Button) findViewById(R.id.buttonDot);
+
+        Button buttonEqual = (Button) findViewById(R.id.buttonEqual);
+        Button buttonDivide = (Button) findViewById(R.id.buttonDivide);
+        Button buttonMultiply = (Button) findViewById(R.id.buttonMultiply);
+        Button buttonPlus = (Button) findViewById(R.id.buttonPlus);
+        Button buttonMinus = (Button) findViewById(R.id.buttonMinus);
     }
 }
